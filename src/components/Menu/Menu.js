97391fc03a9a30be3404067da2b7769205f41a84
Mandy Alford <mandyalford@gmail.com>
2020-03-18 09:57:15 -0600

    Set up initial menu component

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Menu.css'
 
 // MENU COMPONENT CODE GOES HERE
+class Menu extends Component {
+  constructor() {
+    super();
+  }
+
+  render () {
+    return (
+      <nav className='nav-menu'>
+        <p>Local News</p>
+        <p>Technology</p>
+        <p>Entertainment</p>
+        <p>Science</p>
+        <p>Health</p>
+      </nav>
+    )
+  }
+}
 
 export default Menu;
